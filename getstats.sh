d83d1c5a314f3044bc1710008e07afd4ef694407
Chris Wilson <chris+git@aptivate.org>
2013-07-17 11:38:14 +0100

    Add the script to fetch stats from ADSL modem.

--- getstats.sh
+++ getstats.sh
@@ -2,4 +2,10 @@
 
 statsfile=${statsfile:-/var/lib/munin/plugin-state/huawei-hg612-munin-output.txt}
 
-expect `dirname $0`/huawei.expect  | tee | tr -d "\015"  > $statsfile
+expect `dirname $0`/huawei.expect  | tee | tr -d "\015"  > $statsfile.new
+
+if [ -s "$statsfile.new" ]; then
+	mv $statsfile.new $statsfile
+else
+	echo "Empty output from expect script"
+fi
